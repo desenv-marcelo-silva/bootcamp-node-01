8be4b5c7178629e2bfa34b70ad814c23d0c80b8a
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-01-29 21:55:38 -0300

    Delete adjustments. Validate project Middleware.

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const express = require("express");
 
 const server = express();
-server.use(express.json());
 
+server.use(express.json());
 const projetos = [];
+validarProjetoExisteMiddleware = function(req, res, next) {
+  const { id } = req.params;
+  if (projetos.findIndex(projeto => projeto.id == id) > -1) {
+    return next();
+  }
+  return res.status(400).json({ Erro: `Projeto ${id} não existe.` });
+};
 
 server.post("/projects", (req, res) => {
   projetos.push(req.body);
   return res.sendStatus(201);
 });
 
-server.post("/projects/:id/tasks", (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
+server.post(
+  "/projects/:id/tasks",
+  validarProjetoExisteMiddleware,
+  (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
 
-  const projeto = projetos.find(projeto => projeto.id == id);
-  projeto.tasks.push(title);
-  console.table(projetos);
-  return res.sendStatus(201);
-});
+    const projeto = projetos.find(projeto => projeto.id == id);
+    projeto.tasks.push(title);
+    return res.sendStatus(201);
+  }
+);
 
 server.get("/projects", (req, res) => {
   return res.send(projetos);
 });
 
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", validarProjetoExisteMiddleware, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -34,14 +44,11 @@ server.put("/projects/:id", (req, res) => {
   return res.sendStatus(200);
 });
 
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", validarProjetoExisteMiddleware, (req, res) => {
   const { id } = req.params;
+  const projetoIndex = projetos.findIndex(proj => proj.id == id);
 
-  projetos.splice(
-    projetos.find(proj => proj.id == id),
-    1
-  );
-
+  projetos.splice(projetoIndex, 1);
   return res.sendStatus(200);
 });
 
